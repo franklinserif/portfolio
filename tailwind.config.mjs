37ea8ff62b1f8789f373ee6e72391cd08845e6a8
franklin rodriguez <franklinserif@gmail.com>
2024-12-04 17:22:51 -0400

    chore(tailwind.config.mjs): add tailwind theme

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"primary": "#011627",
+			"primary-light": "#01080E",
+			"primary-dark": "#011221",
+			
+			"secondary": "#607B96",
+			"secondary-green": "#3C9D93",
+			"secondary-blue": "#4D5BCE",
+
+			"accent-orange": "#FEA55F",
+			"accent-green": "#43D9AD",
+			"accent-salmon": "#E99287",
+			"accent-violet": "#C98BDF",
+			
+			"lines": "#1E2D3D",
+		},
+		extend: {},
+	},
+	plugins: [],
+}

e72547b33b236adad990466be44849d4709d2225
franklin rodriguez <franklinserif@gmail.com>
2024-12-05 21:25:50 -0400

    chore: add Menu.tsx component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,17 @@
+import type { FC } from 'react'
+
+const Menu: FC = () => {
+  return (
+    <nav>
+      <ul className="text-secondary">
+        <li>Franklin Rodriguez</li>
+        <li>_hello</li>
+        <li>_about-me</li>
+        <li>_projects</li>
+        <li>_contact-me</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu

b0af013ad390a834c7efef9bce3e6d51bf22d91e
franklin rodriguez <franklinserif@gmail.com>
2024-12-05 21:23:52 -0400

    style: format code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			"primary": "#011627",
-			"primary-light": "#01080E",
-			"primary-dark": "#011221",
-			
-			"secondary": "#607B96",
-			"secondary-green": "#3C9D93",
-			"secondary-blue": "#4D5BCE",
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: '#011627',
+      'primary-light': '#01080E',
+      'primary-dark': '#011221',
+      'primary-ultra-dark': '#010C15',
 
-			"accent-orange": "#FEA55F",
-			"accent-green": "#43D9AD",
-			"accent-salmon": "#E99287",
-			"accent-violet": "#C98BDF",
-			
-			"lines": "#1E2D3D",
-		},
-		fontSize: {
-			"headline-text": ["62px"],
-			"subheadline-text": ["32px"],
+      secondary: '#607B96',
+      'secondary-green': '#3C9D93',
+      'secondary-blue': '#4D5BCE',
 
-			body: ["18px"],
-			labels: ["16px"],
-			"code-snippets": ["14px"]
-		},
-		extend: {},
-	},
-	plugins: [],
+      'accent-orange': '#FEA55F',
+      'accent-green': '#43D9AD',
+      'accent-salmon': '#E99287',
+      'accent-violet': '#C98BDF',
+
+      lines: '#1E2D3D',
+    },
+    fontSize: {
+      'headline-text': ['62px'],
+      'subheadline-text': ['32px'],
+
+      body: ['18px'],
+      labels: ['16px'],
+      'code-snippets': ['14px'],
+    },
+    extend: {},
+  },
+  plugins: [],
 }

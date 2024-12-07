939dcf13db8d52cc2a0b1c50701dad371607f35f
franklin rodriguez <franklinserif@gmail.com>
2024-12-07 14:57:39 -0400

    chore(astro.config.mjs): add vue and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
+
+import vue from '@astrojs/vue'
+
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  integrations: [tailwind(), react(), vue()],
+  adapter: vercel(),
+})

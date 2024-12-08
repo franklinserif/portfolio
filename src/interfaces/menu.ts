d77a9c4b1dd61efdd595787589c23b6e5a3406fa
franklin rodriguez <franklinserif@gmail.com>
2024-12-08 18:06:13 -0400

    feat(interfaces): add new types

--- src/interfaces/menu.ts
+++ src/interfaces/menu.ts
@@ -1,1 +1,18 @@
 export type MenuOptions = '/' | '/about-me' | '/projects' | '/contact-me'
+
+export type TSubMenuOptions =
+  | 'personal-info'
+  | 'professional-info'
+  | 'hobbies-info'
+
+export interface ISubMenuOption {
+  id: number
+  name: string
+  url: string
+  icon: string
+  arrow: boolean
+}
+
+export type ISubMenu = {
+  [K in TSubMenuOptions]: ISubMenuOption[]
+}

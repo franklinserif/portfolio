86bc5ac2375ee396018d039d59f034bff126306e
franklin rodriguez <franklinserif@gmail.com>
2024-12-24 13:43:24 -0400

    feat(libs): add getTitle function

---
+++ src/libs/getTitle.ts
@@ -0,0 +1,19 @@
+function getTitle(pathname: string): string[] {
+  const splitTitle = pathname.split('/')
+  const title = splitTitle[2]
+  let subTitle = splitTitle[3]
+
+  console.log('split title: ', splitTitle)
+
+  if (subTitle === undefined && title == 'personal-info') {
+    subTitle = 'bio'
+  } else if (subTitle === undefined && title === 'professional-info') {
+    subTitle = 'experience'
+  } else if (subTitle === undefined && title === 'hobbies-info') {
+    subTitle = 'sports'
+  }
+
+  return [splitTitle[2], subTitle]
+}
+
+export { getTitle }

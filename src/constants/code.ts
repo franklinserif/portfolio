e8d1a51acc95356977f5b9086a5b24fcfefea3b1
franklin rodriguez <franklinserif@gmail.com>
2024-12-26 17:26:02 -0400

    feat(constants): code

---
+++ src/constants/code.ts
@@ -0,0 +1,9 @@
+export const javscriptCode1 = `
+  function initializeModelChunk<T>(chunk: ResolvedModelChunk): T {
+  const value: T = parseModel(chunk._response, chunk._value);
+  const initializedChunk: InitializedChunk<T> = (chunk: any);
+  initializedChunk._status = INITIALIZED;
+  initializedChunk._value = value;
+  return value;
+}
+`

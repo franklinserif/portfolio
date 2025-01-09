266245ad17354270a07249546019b1cd9a212b3b
franklin rodriguez <franklinserif@gmail.com>
2025-01-09 12:25:34 -0400

    refactor(constants/code): change name of variable and add EG_CODE_JAVASCRIPT_2

--- src/constants/code.ts
+++ src/constants/code.ts
@@ -1,4 +1,4 @@
-export const javscriptCode1 = `
+export const EG_CODE_JAVASCRIPT_1 = `
   function initializeModelChunk<T>(chunk: ResolvedModelChunk): T {
   const value: T = parseModel(chunk._response, chunk._value);
   const initializedChunk: InitializedChunk<T> = (chunk: any);
@@ -6,4 +6,12 @@ export const javscriptCode1 = `
   initializedChunk._value = value;
   return value;
 }
+`
+
+export const EG_CODE_JAVASCRIPT_2 = `
+export function parseModelTuple(
+  response: Response,
+  value: {+[key: string]: JSONValue} | $ReadOnlyArray<JSONValue>,
+): any {
+  const tuple: [mixed, mixed, mixed, mixed] = (value: any);
 `

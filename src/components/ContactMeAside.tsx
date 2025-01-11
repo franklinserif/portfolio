3d4beb7585880e6c01cd9cf13f2b720477adc754
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 14:07:38 -0400

    feat(components): :sparkles: add ContactMeAside.tsx

--- src/components/ContactMeAside.tsx
+++ src/components/ContactMeAside.tsx
@@ -1,7 +1,44 @@
-import type { FC } from 'react'
+import { type FC } from 'react'
+import { useContactFormStore } from 'src/store/useContactFormStore'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const ContactMeAside: FC = () => {
-  return <aside>aside</aside>
+  const form = useContactFormStore((state) => state.form)
+
+  const formCode = `
+    const button = document.querySelector ( '#sendBtn' );
+
+
+    const message = {
+      name : "${form.name}" ,
+      email : "${form.email}" ,
+      message : "${form.message}" ,
+      date: "Sat Jan 11 2025"
+    }
+
+
+    button.addEventListener ( 'click' ), () => {
+      form.send ( message );
+    })
+  `
+
+  return (
+    <aside className="p-5">
+      <SyntaxHighlighter
+        showLineNumbers={true}
+        language="javascript"
+        style={atomOneDark}
+        customStyle={{
+          background: 'transparent',
+          fontSize: '15px',
+          overflow: 'hidden',
+        }}
+      >
+        {formCode}
+      </SyntaxHighlighter>
+    </aside>
+  )
 }
 
 export default ContactMeAside

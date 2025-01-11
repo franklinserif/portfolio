dfb28528c4af0fac064820ae9a970f15b00ba1bf
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 14:08:08 -0400

    refactor(SubMenu.tsx): :recycle: implement logic for contact-me page

--- src/components/SubMenu/SubMenu.tsx
+++ src/components/SubMenu/SubMenu.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { clsx } from 'clsx'
 import SubMenuItem from '@components/SubMenu/SubMenuItem'
 import MobileSubMenu from '@components/SubMenu/MobileSubMenu'
 import { SUB_MENU, SUB_MENU_CONTACT } from '@constants/menus'
@@ -9,14 +10,20 @@ interface Props {
 }
 
 const SubMenu: FC<Props> = ({ pathname }) => {
-  const path = pathname?.split('/')[2] as TSubMenuOptions
+  const path =
+    (pathname?.split('/')[2] as TSubMenuOptions) ||
+    (pathname?.split('/')[1] as TSubMenuOptions)
 
   const menuList = SUB_MENU[path]
 
   const subMenuList = Object.entries(SUB_MENU)
 
   return (
-    <section className="min-w-[248px] border-r border-r-lines">
+    <section
+      className={clsx('min-w-[248px] border-r border-r-lines', {
+        'min-w-[310px]': path === 'contact-me',
+      })}
+    >
       <nav className="hidden xl:block">
         <div className="h-[35px] border-b border-b-lines flex items-center">
           <img
@@ -31,19 +38,23 @@ const SubMenu: FC<Props> = ({ pathname }) => {
             <SubMenuItem key={item.id} item={item} pathname={pathname} />
           ))}
         </ul>
-        <div className="h-[35px] border-y border-y-lines flex items-center mt-6">
-          <img
-            src="/icons/white-arrow-icon.svg"
-            className="w-2 mx-2 ml-3"
-            alt="white arrow"
-          />
-          <h4 className="text-white text-snippets">contacts</h4>
-        </div>
-        <ul className="flex flex-col gap-3 ml-3 pt-3">
-          {SUB_MENU_CONTACT.map((item) => (
-            <SubMenuItem key={item.id} item={item} pathname={pathname} />
-          ))}
-        </ul>
+        {path !== 'contact-me' && (
+          <div className="h-[35px] border-y border-y-lines flex items-center mt-6">
+            <img
+              src="/icons/white-arrow-icon.svg"
+              className="w-2 mx-2 ml-3"
+              alt="white arrow"
+            />
+            <h4 className="text-white text-snippets">contacts</h4>
+          </div>
+        )}
+        {path !== 'contact-me' && (
+          <ul className="flex flex-col gap-3 ml-3 pt-3">
+            {SUB_MENU_CONTACT.map((item) => (
+              <SubMenuItem key={item.id} item={item} pathname={pathname} />
+            ))}
+          </ul>
+        )}
       </nav>
       <nav className="block xl:hidden w-full">
         {subMenuList.map((item, index) => (

13d0369ba7aceda88a6f151e0590c0844e8739e9
franklin rodriguez <franklinserif@gmail.com>
2025-01-11 14:07:22 -0400

    feat(components): :sparkles: add ContactMeForm

---
+++ src/components/ContactMeForm.tsx
@@ -0,0 +1,56 @@
+import type { FC } from 'react'
+import { useContactFormStore } from '@store/useContactFormStore'
+
+const ContactMeForm: FC = () => {
+  const formState = useContactFormStore((state) => state)
+
+  const sendEmail = () => {
+    const { form } = formState
+    if (!form.email || !form.email || form.message) return
+
+    const mailtoLink = `mailto:${form.email}?subject=${encodeURIComponent(form.name)}&body=${encodeURIComponent(form.message)}`
+    window.location.href = mailtoLink
+  }
+
+  return (
+    <div className="w-full flex justify-center items-center">
+      <form action="" className="flex flex-col max-w-[400px] w-full mt-16">
+        <label htmlFor="name" className="text-snippets text-secondary mb-3">
+          _name:
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="bg-primary-dark rounded-md p-2 border-2 border-lines outline-none mb-7 focus:border-secondary text-secondary text-snippets"
+          onChange={(event) => formState.changeName(event.target.value)}
+        />
+        <label htmlFor="email" className="text-snippets text-secondary mb-3">
+          _email:
+        </label>
+        <input
+          type="text"
+          id="email"
+          className="bg-primary-dark rounded-md p-2 border-2 border-lines outline-none mb-7 focus:border-secondary text-secondary text-snippets"
+          onChange={(event) => formState.changeEmail(event.target.value)}
+        />
+        <label htmlFor="message" className="text-snippets text-secondary mb-3">
+          _message:
+        </label>
+        <textarea
+          id="name"
+          className="bg-primary-dark rounded-md p-2 border-2 border-lines outline-none mb-7 focus:border-secondary text-secondary text-snippets"
+          onChange={(event) => formState.changeMessage(event.target.value)}
+          rows={7}
+        />
+        <button
+          onClick={sendEmail}
+          className="bg-lines text-white text-small-letter py-2 px-3 rounded-md w-fit"
+        >
+          submit-message
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default ContactMeForm
